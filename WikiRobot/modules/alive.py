a07ed9eda78116d515553be97221cea78bcc3033
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-20 19:54:45 +0800

    Update alive.py

--- WikiRobot/modules/alive.py
+++ WikiRobot/modules/alive.py
@@ -5,20 +5,20 @@ from telethon import events, Button
 from telegram import __version__ as telever
 from telethon import __version__ as tlhver
 from pyrogram import __version__ as pyrover
-from EmikoRobot.events import register
-from EmikoRobot import telethn as tbot
+from WikiRobot.events import register
+from WikiRobot import telethn as tbot
 
 
-PHOTO = "https://telegra.ph/file/2c13e570944404a6c0d86.jpg"
+PHOTO = "https://telegra.ph/file/9b9a27cd02e65046d5515.jpg"
 
 @register(pattern=("/alive"))
 async def awake(event):
-  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
-  TEXT += "⚪ **I'm Working Properly** \n\n"
-  TEXT += f"⚪ **My Master : [sena-ex](https://t.me/excrybaby)** \n\n"
+  TEXT = f"**Hai [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Emiko Robot.** \n\n"
+  TEXT += "⚪ **Saya Bekerja Dengan Benar** \n\n"
+  TEXT += f"⚪ **Tuanku : [Wiki W](WikiTapiOrang)** \n\n"
   TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
   TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
   TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
-  TEXT += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("Help", "https://t.me/EmiexRobot?start=help"), Button.url("Support", "https://t.me/emikosupport")]]
+  TEXT += "**Terima Kasih Telah Menambahkan Saya Di Sini ❤️**"
+  BUTTON = [[Button.url("Help", "https://t.me/WikiTapiBot?start=help"), Button.url("Support", "https://t.me/WikiTapiGroup")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
