2aa6b35dfc427a71a67a65dc4946346494038177
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-25 06:24:28 +0800

    Update dbcleanup.py

--- WikiRobot/modules/dbcleanup.py
+++ WikiRobot/modules/dbcleanup.py
@@ -1,9 +1,9 @@
 from time import sleep
 
-import EmikoRobot.modules.sql.global_bans_sql as gban_sql
-import EmikoRobot.modules.sql.users_sql as user_sql
-from EmikoRobot import DEV_USERS, OWNER_ID, dispatcher
-from EmikoRobot.modules.helper_funcs.chat_status import dev_plus
+import WikiRobot.modules.sql.global_bans_sql as gban_sql
+import WikiRobot.modules.sql.users_sql as user_sql
+from WikiRobot import DEV_USERS, OWNER_ID, dispatcher
+from WikiRobot.modules.helper_funcs.chat_status import dev_plus
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.error import BadRequest, Unauthorized
 from telegram.ext import (
