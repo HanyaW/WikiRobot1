74dd48943daa314f7be2d40189faf32178d96a13
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-24 12:49:24 +0800

    Update urbandictionary.py

--- WikiRobot/modules/urbandictionary.py
+++ WikiRobot/modules/urbandictionary.py
@@ -1,6 +1,6 @@
 import requests
-from EmikoRobot import dispatcher
-from EmikoRobot.modules.disable import DisableAbleCommandHandler
+from WikiRobot import dispatcher
+from WikiRobot.modules.disable import DisableAbleCommandHandler
 from telegram import ParseMode, Update
 from telegram.ext import CallbackContext, run_async
 
