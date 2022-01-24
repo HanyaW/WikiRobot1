a18b3f89dc2e72bfde6be88451f86276d2b77c14
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-25 06:30:03 +0800

    Update core.py

--- WikiRobot/modules/core.py
+++ WikiRobot/modules/core.py
@@ -5,11 +5,11 @@ import asyncio
 import os
 import time
 from datetime import datetime
-from EmikoRobot import OWNER_ID, DEV_USERS
-from EmikoRobot import TEMP_DOWNLOAD_DIRECTORY as path
-from EmikoRobot import TEMP_DOWNLOAD_DIRECTORY
+from WikiRobot import OWNER_ID, DEV_USERS
+from WikiRobot import TEMP_DOWNLOAD_DIRECTORY as path
+from WikiRobot import TEMP_DOWNLOAD_DIRECTORY
 from datetime import datetime
-water = './EmikoRobot/resources/emiko.png'
+water = './WikiRobot/resources/emiko.png'
 client = tbot
 
 @register(pattern=r"^/send ?(.*)")
@@ -21,7 +21,7 @@ async def Prof(event):
     thumb = water
     message_id = event.message.id
     input_str = event.pattern_match.group(1)
-    the_plugin_file = "./EmikoRobot/modules/{}.py".format(input_str)
+    the_plugin_file = "./WikiRobot/modules/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
      message_id = event.message.id
      await event.client.send_file(
