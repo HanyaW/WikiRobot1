3876fef5ea674bd7e02e6fa5939ca735fde7aeae
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-24 16:56:26 +0800

    Update sangmata.py

--- WikiRobot/modules/sangmata.py
+++ WikiRobot/modules/sangmata.py
@@ -1,7 +1,7 @@
 from telethon.errors.rpcerrorlist import YouBlockedUserError
-from EmikoRobot import telethn as tbot
-from EmikoRobot.events import register
-from EmikoRobot import ubot2 as ubot
+from WikiRobot import telethn as tbot
+from WikiRobot.events import register
+from WikiRobot import ubot2 as ubot
 from asyncio.exceptions import TimeoutError
 
 
