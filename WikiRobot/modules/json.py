9ed73a11c559e41aec1d8fb23619670d32993511
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-24 19:15:17 +0800

    Update json.py

--- WikiRobot/modules/json.py
+++ WikiRobot/modules/json.py
@@ -1,6 +1,6 @@
 import io
-from EmikoRobot.events import register
-from EmikoRobot import telethn as tbot
+from WikiRobot.events import register
+from WikiRobot import telethn as tbot
 from telethon import types
 from telethon import events
 from telethon.tl import functions
