a81f719ee4b39d869e7a4f11894e5e29551a0269
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-24 13:22:49 +0800

    Update tools.py

--- WikiRobot/modules/tools.py
+++ WikiRobot/modules/tools.py
@@ -2,36 +2,36 @@ __mod_name__ = "Tools"
 
 __help__ = """
 *Sangmata Info bot*
-❂ /sg <reply>*:* To check history name
+❂ /sg <reply>*:* Untuk memeriksa nama riwayat
 
 *Date-time-Weather*
-❂ /time <country code>*:* Gives information about a timezone.
-❂ /weather <city>*:* Get weather info in a particular place.
-❂ /wttr <city>*:* Advanced weather module, usage same as /weather
-❂ /wttr moon*:* Get the current status of moon
+❂ /time <country code>*:* Memberikan informasi tentang zona waktu.
+❂ /weather <city>*:* Dapatkan info cuaca di tempat tertentu.
+❂ /wttr <city>*:* Modul cuaca lanjutan, penggunaan sama seperti /weather
+❂ /wttr moon*:* Dapatkan status bulan saat ini
 
 *Converts*
-❂ /encrypt*:* Encrypts The Given Text
-❂ /decrypt*:* Decrypts Previously Ecrypted Text
-❂ /zip*:* reply to a telegram file to compress it in .zip format
-❂ /unzip*:* reply to a telegram file to decompress it from the .zip format
+❂ /encrypt*:* Mengenkripsi Teks yang Diberikan
+❂ /decrypt*:* Mendekripsi Teks Terenkripsi Sebelumnya
+❂ /zip*:* membalas file telegram untuk mengompresnya .zip format
+❂ /unzip*:* membalas file telegram untuk mendekompresnya dari .zip format
 
 *Random API*
-❂ /ptl or /asupan: get random video from Instagram timeline
-❂ /chika: get random video from chikakiku
-❂ /wibu:get random short anime video or photos
+❂ /ptl or /asupan: dapatkan video acak dari timeline Instagram
+❂ /chika: dapatkan video acak dari chikakiku
+❂ /wibu: dapatkan video atau foto anime pendek acak
 ❂ /truth: challenge 
 ❂ /dare: challenge
 Link To File:
-❂ /up: reply to a direct download link to upload it to telegram as files
+❂ /up: balas tautan unduhan langsung untuk mengunggahnya ke telegram sebagai file
  
  File To Link:
-❂ /transfersh: reply to a telegram file to upload it on transfersh and get direct download link
-❂ /tmpninja: reply to a telegram file to upload it on tmpninja and get direct download link
+❂ /transfersh: balas file telegram untuk mengunggahnya di transfersh dan dapatkan tautan unduhan langsung
+❂ /tmpninja: balas file telegram untuk mengunggahnya di tmpninja dan dapatkan tautan unduhan langsung
  File Or Text To Telegraph:
-❂ /tgm : Get Telegraph Link Of Replied Media
-❂ /tgt: Get Telegraph Link of Replied Text
+❂ /tgm : Dapatkan Tautan Telegraf Media Balasan
+❂ /tgt: Dapatkan Tautan Telegraf dari Teks Balasan
 
  Tagger
-❂ /tagall : for tag everyone at least 100 member first.
+❂ /tagall : untuk tag semua orang minimal 100 member dulu.
 """
