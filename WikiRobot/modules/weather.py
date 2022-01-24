8add121a79117cb4f8b2ac62bd6e374f35fa5e6e
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-24 12:37:34 +0800

    Update weather.py

--- WikiRobot/modules/weather.py
+++ WikiRobot/modules/weather.py
@@ -8,10 +8,10 @@ from telethon.tl import functions
 from telethon.tl import types
 from telethon.tl.types import *
 
-from EmikoRobot import *
+from WikiRobot import *
 
-from EmikoRobot import telethn as tbot
-from EmikoRobot.events import register
+from WikiRobot import telethn as tbot
+from WikiRobot.events import register
 
 
 async def is_register_admin(chat, user):
