96a53dceb37cf0fcd81083022d1d6be5be938a1e
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-24 16:46:04 +0800

    Update tiny.py

--- WikiRobot/modules/tiny.py
+++ WikiRobot/modules/tiny.py
@@ -1,8 +1,8 @@
 import os
 import cv2
 from PIL import Image
-from EmikoRobot.events import register
-from EmikoRobot import telethn as tbot
+from WikiRobot.events import register
+from WikiRobot import telethn as tbot
 
 
 @register(pattern="^/tiny ?(.*)")
