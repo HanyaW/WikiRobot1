c6a2d0113611fd8b5421001cebc079d29606248c
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-24 19:08:26 +0800

    Update logomaker.py

--- WikiRobot/modules/logomaker.py
+++ WikiRobot/modules/logomaker.py
@@ -3,8 +3,8 @@ import random
 import glob
 from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.types import InputMessagesFilterPhotos
-from EmikoRobot.events import register
-from EmikoRobot import telethn as tbot, ubot2
+from WikiRobot.events import register
+from WikiRobot import telethn as tbot, ubot2
 
 
 def mediainfo(media):
@@ -68,7 +68,7 @@ async def logo_gen(event):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
-        fpath_ = glob.glob("./EmikoRobot/resources/fonts/*")
+        fpath_ = glob.glob("./WikiRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if not bg_:
         pics = []
@@ -79,7 +79,7 @@ async def logo_gen(event):
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
     if not font_:
-        fpath_ = glob.glob("./EmikoRobot/resources/fonts/*")
+        fpath_ = glob.glob("./WikiRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if len(name) <= 8:
         fnt_size = 120
@@ -104,8 +104,9 @@ async def logo_gen(event):
     )
     x = (image_width - w) / 2
     y = (image_height - h) / 2
-    draw.text((x, y), name, font=font, fill="white",
-              stroke_width=strke, stroke_fill="black")
+    draw.text(
+        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
+    )
     flnme = f"logo.png"
     img.save(flnme, "png")
     await xx.edit("`Uploading`")
@@ -113,15 +114,15 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Emiko Robot](https://t.me/EmiexRobot)",
+            caption="Logo by [Wiki W](https://t.me/WikiTapiRobot)",
             force_document=False,
         )
         os.remove(flnme)
         await xx.delete()
     if os.path.exists(bg_):
-        os.remove(bg_) 
+        os.remove(bg_)
     if os.path.exists(font_):
-        if not font_.startswith("./EmikoRobot/resources/fonts"):
+        if not font_.startswith("./WikiRobot/resources/fonts"):
             os.remove(font_)
 
 
@@ -151,7 +152,7 @@ async def logo_(event):
             pics.append(i)
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
-        fpath_ = glob.glob("./EmikoRobot/resources/fonts/*")
+        fpath_ = glob.glob("./WikiRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if not bg_:
         pics = []
@@ -162,7 +163,7 @@ async def logo_(event):
         id_ = random.choice(pics)
         bg_ = await id_.download_media()
     if not font_:
-        fpath_ = glob.glob("./EmikoRobot/resources/fonts/*")
+        fpath_ = glob.glob("./WikiRobot/resources/fonts/*")
         font_ = random.choice(fpath_)
     if len(name) <= 8:
         fnt_size = 105
@@ -187,8 +188,9 @@ async def logo_(event):
     )
     x = (image_width - w) / 2
     y = (image_height - h) / 2
-    draw.text((x, y), name, font=font, fill="white",
-              stroke_width=strke, stroke_fill="black")
+    draw.text(
+        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
+    )
     flnme = f"logo.png"
     img.save(flnme, "png")
     await xx.edit("`Uploading`")
@@ -196,15 +198,98 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Emiko Robot](https://t.me/EmiexRobot)",
+            caption="Logo by [Wiki W](https://t.me/WikiTapiRobot)",
             force_document=False,
         )
         os.remove(flnme)
         await xx.delete()
     if os.path.exists(bg_):
-        os.remove(bg_) 
+        os.remove(bg_)
     if os.path.exists(font_):
-        if not font_.startswith("./EmikoRobot/resources/fonts"):
+        if not font_.startswith("./WikiRobot/resources/fonts"):
+            os.remove(font_)
+
+@register(pattern="^/clogo ?(.*)")
+async def logo_(event):
+    xx = await event.reply("`Preparing your logo...`")
+    name = event.pattern_match.group(1)
+    if not name:
+        await xx.edit("`Provide some text to draw!\nExample: /wlogo <your name>!`")
+        return
+    bg_, font_ = "", ""
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            if hasattr(temp.media, "document"):
+                if "font" in temp.file.mime_type:
+                    font_ = await temp.download_media()
+                elif (".ttf" in temp.file.name) or (".otf" in temp.file.name):
+                    font_ = await temp.download_media()
+            elif "pic" in mediainfo(temp.media):
+                bg_ = await temp.download_media()
+    else:
+        pics = []
+        async for i in ubot2.iter_messages(
+            "@WikiLogoCecan", filter=InputMessagesFilterPhotos
+        ):
+            pics.append(i)
+        id_ = random.choice(pics)
+        bg_ = await id_.download_media()
+        fpath_ = glob.glob("./WikiRobot/resources/fonts/*")
+        font_ = random.choice(fpath_)
+    if not bg_:
+        pics = []
+        async for i in ubot2.iter_messages(
+            "@WikiLogoCecan", filter=InputMessagesFilterPhotos
+        ):
+            pics.append(i)
+        id_ = random.choice(pics)
+        bg_ = await id_.download_media()
+    if not font_:
+        fpath_ = glob.glob("./WikiRobot/resources/fonts/*")
+        font_ = random.choice(fpath_)
+    if len(name) <= 8:
+        fnt_size = 105
+        strke = 8
+    elif len(name) >= 9:
+        fnt_size = 50
+        strke = 4
+    else:
+        fnt_size = 95
+        strke = 13
+    img = Image.open(bg_)
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype(font_, fnt_size)
+    w, h = draw.textsize(name, font=font)
+    h += int(h * 0.21)
+    image_width, image_height = img.size
+    draw.text(
+        ((image_width - w) / 2, (image_height - h) / 2),
+        name,
+        font=font,
+        fill=(255, 255, 255),
+    )
+    x = (image_width - w) / 2
+    y = (image_height - h) / 2
+    draw.text(
+        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
+    )
+    flnme = f"logo.png"
+    img.save(flnme, "png")
+    await xx.edit("`Uploading`")
+    if os.path.exists(flnme):
+        await tbot.send_file(
+            event.chat_id,
+            file=flnme,
+            caption="Logo by [Wiki W](https://t.me/WikiTapiRobot)",
+            force_document=False,
+        )
+        os.remove(flnme)
+        await xx.delete()
+    if os.path.exists(bg_):
+        os.remove(bg_)
+    if os.path.exists(font_):
+        if not font_.startswith("./WikiRobot/resources/fonts"):
             os.remove(font_)
 
 
@@ -212,10 +297,10 @@ __mod_name__ = "Logomaker"
 
 __help__ = """ This is help menu for logomaker
 
-❂ /logo <text/name> - Create a logo with random view.
-❂ /wlogo <text/name> - Create a logo with wide view only.
-
+❂ /logo <text/name> - Buat logo dengan tampilan acak.
+❂ /wlogo <text/name> - Buat logo dengan tampilan lebar saja.
+❂ /clogo <text/name> - Buat logo dengan tampilan cewe cantik
  Image Editor :
 
-❂  /edit <reply photo> - to edit image.
+❂  /edit <reply photo> - untuk mengedit gambar.
 """
