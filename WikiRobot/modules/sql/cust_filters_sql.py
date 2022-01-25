bd5a1997b334eb6b60c2ed70592c86879b5cca93
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-25 09:41:55 +0800

    Update cust_filters_sql.py

--- WikiRobot/modules/sql/cust_filters_sql.py
+++ WikiRobot/modules/sql/cust_filters_sql.py
@@ -2,8 +2,8 @@ import threading
 
 from sqlalchemy import Column, String, UnicodeText, Boolean, distinct, func, Integer
 
-from EmikoRobot.modules.helper_funcs.msg_types import Types
-from EmikoRobot.modules.sql import BASE, SESSION
+from WikiRobot.modules.helper_funcs.msg_types import Types
+from WikiRobot.modules.sql import BASE, SESSION
 
 
 class CustomFilters(BASE):
