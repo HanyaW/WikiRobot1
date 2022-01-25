4f10ca09a22b5847e872a94314469862e58341f8
Wiki28 <93371415+Wiki28@users.noreply.github.com>
2022-01-25 09:47:52 +0800

    Update chatstatus.py

--- WikiRobot/modules/helper_funcs/telethn/chatstatus.py
+++ WikiRobot/modules/helper_funcs/telethn/chatstatus.py
@@ -1,5 +1,5 @@
-from EmikoRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
-from EmikoRobot import DRAGONS
+from WikiRobot.modules.helper_funcs.telethn import IMMUNE_USERS, telethn
+from WikiRobot import DRAGONS
 from telethon.tl.types import ChannelParticipantsAdmins
 
 
